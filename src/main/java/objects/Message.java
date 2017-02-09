10a0f501b6b9438dd12d3cc8cbc4a81aad48b887
patrickeddy <p@patrickeddy.com>
2017-02-08 20:35:49 -0800

    Broken user class fix.

---
+++ src/main/java/objects/Message.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objects;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author patrickeddy
+ */
+public class Message {
+    private int messageId;
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getDateadded() {
+        return dateadded;
+    }
+
+    public void setDateadded(Date dateadded) {
+        this.dateadded = dateadded;
+    }
+    private int userId;
+    private String message;
+    private Date dateadded;
+    
+    
+}
